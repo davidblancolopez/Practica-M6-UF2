53c624c92da032b93e53938d3249ee04a7f03593
davidblancolopez <daviddbl94@gmail.com>
2017-02-28 20:26:17 +0100

    CAMBIOS

--- src/controlador/Polissa_Controlador.java
+++ src/controlador/Polissa_Controlador.java
@@ -1,7 +1,15 @@
 
 package controlador;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import model.Client;
+
 
 public class Polissa_Controlador {
+    
+    
     
 }
