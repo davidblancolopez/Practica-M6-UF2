a780501f9a2a44d18a4cf46912649fc90623555d
davidblancolopez <daviddbl94@gmail.com>
2017-03-02 17:23:41 +0100

    Añadida QUERY en Client.

---
+++ src/controlador/Client_Controlador.java
@@ -0,0 +1,134 @@
+
+package controlador;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
+import model.Asseguradora;
+import model.Client;
+import model.Usuari;
+
+
+public class Client_Controlador {
+    
+    public void Insertar(Client c) {
+        // Recupera el entity manager
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        // El persistim a la base de dades
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("persist");
+        em.persist(c);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void Modificar(Client c) {
+        // Recupera el entity manager
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        // El persistim a la base de dades
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("merge");
+        em.merge(c);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void Eliminar(Client c) {
+        // Recupera el entity manager
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        // El persistim a la base de dades
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("remove");
+        em.remove(em.contains(c) ? c : em.merge(c));
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public Client Buscar(Long id) {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("busqueda");
+
+        Client p = (Client) em.find(Client.class, id);
+
+        System.out.println("close");
+        em.close();
+
+        return p;
+    }
+    
+    public Client BuscarPerNom(String nom) {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("Busqueda per nom");
+        //Query query = em.createNamedQuery("PersonaNom",Persona.class);
+        Query query = em.createNamedQuery(Usuari.consulta,Usuari.class);
+        query.setParameter("nombre", nom);
+        Client p = (Client) query.getSingleResult();
+
+        System.out.println("close");
+        em.close();
+
+        return p;
+    }
+
+    public void Consulta() {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("Consulta");
+        //List<Persona> lista = (List<Persona>) em.createQuery("FROM Persona").getResultList();
+        Query q = em.createQuery("FROM Asseguradora");
+        List<Client> lista = (List<Client>) q.getResultList();
+        imprimirLista(lista);
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void imprimirLista(List<Client> lista) {
+        System.out.println("Numero de clients= " + lista.size());
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }
+    }
+}
