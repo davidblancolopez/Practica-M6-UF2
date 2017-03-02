9e609479003e371a99b71618fa826aa1014fe3cc
davidblancolopez <daviddbl94@gmail.com>
2017-03-02 16:03:08 +0100

    CAMBIOS

---
+++ src/PersistenciaJPA/PersistenciaClient.java
@@ -0,0 +1,22 @@
+package PersistenciaJPA;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import model.Client;
+
+public class PersistenciaClient {
+
+    private EntityManager em;
+
+    public PersistenciaClient(EntityManager em) {
+        this.em = em;
+    }
+
+    public List<Client> cercaPerNom(String nom) {
+        Query q = em.createNamedQuery("Persona.cercaPerNom", Client.class);
+        q.setParameter("nomP", nom);
+        return q.getResultList();
+    }
+
+}
