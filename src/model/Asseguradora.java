e424c6b48f17e1a893144086fb3a7e32bcffe2bc
davidblancolopez <daviddbl94@gmail.com>
2017-03-03 20:49:00 +0100

    Añadida QUERY en Client.

--- src/model/Asseguradora.java
+++ src/model/Asseguradora.java
@@ -3,15 +3,13 @@ package model;
 
 import java.io.Serializable;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+
 
 
 @Entity
