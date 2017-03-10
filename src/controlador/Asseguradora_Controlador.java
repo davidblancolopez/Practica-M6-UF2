5dcde92a6cd3c7102c840298b1644af05a3f3cfe
davidblancolopez <daviddbl94@gmail.com>
2017-03-10 20:29:00 +0100

    Añadida QUERY en Client.

--- src/controlador/Asseguradora_Controlador.java
+++ src/controlador/Asseguradora_Controlador.java
@@ -33,100 +33,99 @@ public class Asseguradora_Controlador {
         em.close();
     }
 
-//    public void Modificar(Asseguradora a) {
-//        // Recupera el entity manager
-//        EM_Controller oem = new EM_Controller();
-//        EntityManager em = oem.getEntityManager();
-//
-//        // El persistim a la base de dades
-//        //em.getTransaction().begin();
-//        EntityTransaction etx = em.getTransaction();
-//
-//        System.out.println("begin");
-//        etx.begin();
-//
-//        System.out.println("merge");
-//        em.merge(a);
-//
-//        System.out.println("commit");
-//        //em.getTransaction().commit();
-//        etx.commit();
-//
-//        System.out.println("close");
-//        em.close();
-//    }
-//
-//    public void Eliminar(Asseguradora a) {
-//        // Recupera el entity manager
-//        EM_Controller oem = new EM_Controller();
-//        EntityManager em = oem.getEntityManager();
-//
-//        // El persistim a la base de dades
-//        //em.getTransaction().begin();
-//        EntityTransaction etx = em.getTransaction();
-//
-//        System.out.println("begin");
-//        etx.begin();
-//
-//        System.out.println("remove");
-//        em.remove(em.contains(a) ? a : em.merge(a));
-//
-//        System.out.println("commit");
-//        //em.getTransaction().commit();
-//        etx.commit();
-//
-//        System.out.println("close");
-//        em.close();
-//    }
-//
-//    public Asseguradora Buscar(Long id) {
-//        // Recupera el entity manager
-//        EntityManager em = new EM_Controller().getEntityManager();
-//
-//        System.out.println("busqueda");
-//
-//        Asseguradora p = (Asseguradora) em.find(Asseguradora.class, id);
-//
-//        System.out.println("close");
-//        em.close();
-//
-//        return p;
-//    }
-//    
-//    public Asseguradora BuscarPerNom(String nom) {
-//        // Recupera el entity manager
-//        EntityManager em = new EM_Controller().getEntityManager();
-//
-//        System.out.println("Busqueda per nom");
-//        //Query query = em.createNamedQuery("PersonaNom",Persona.class);
-//        Query query = em.createNamedQuery(Usuari.consulta,Usuari.class);
-//        query.setParameter("nombre", nom);
-//        Asseguradora p = (Asseguradora) query.getSingleResult();
-//
-//        System.out.println("close");
-//        em.close();
-//
-//        return p;
-//    }
-//
-//    public void Consulta() {
-//        // Recupera el entity manager
-//        EntityManager em = new EM_Controller().getEntityManager();
-//
-//        System.out.println("Consulta");
-//        //List<Persona> lista = (List<Persona>) em.createQuery("FROM Persona").getResultList();
-//        Query q = em.createQuery("FROM Asseguradora");
-//        List<Asseguradora> lista = (List<Asseguradora>) q.getResultList();
-//        imprimirLista(lista);
-//
-//        System.out.println("close");
-//        em.close();
-//    }
-//
-//    public void imprimirLista(List<Asseguradora> lista) {
-//        System.out.println("Numero d'asseguradores= " + lista.size());
-//        for (int i = 0; i < lista.size(); i++) {
-//            System.out.println(lista.get(i));
-//        }
-//    }
+    public void Modificar(Asseguradora a) {
+        // Recupera el entity manager
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        // El persistim a la base de dades
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("merge");
+        em.merge(a);
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void Eliminar(Asseguradora a) {
+        // Recupera el entity manager
+        EM_Controller oem = new EM_Controller();
+        EntityManager em = oem.getEntityManager();
+
+        // El persistim a la base de dades
+        //em.getTransaction().begin();
+        EntityTransaction etx = em.getTransaction();
+
+        System.out.println("begin");
+        etx.begin();
+
+        System.out.println("remove");
+        em.remove(em.contains(a) ? a : em.merge(a));
+
+        System.out.println("commit");
+        //em.getTransaction().commit();
+        etx.commit();
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public Asseguradora Buscar(Long id) {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("busqueda");
+
+        Asseguradora p = (Asseguradora) em.find(Asseguradora.class, id);
+
+        System.out.println("close");
+        em.close();
+
+        return p;
+    }
+    
+    public Asseguradora BuscarPerNom(String nom) {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("Busqueda per nom");
+        //Query query = em.createNamedQuery("PersonaNom",Persona.class);
+        Query query = em.createNamedQuery("ClientNom",Asseguradora.class);
+        query.setParameter("nom", nom);
+        Asseguradora p = (Asseguradora) query.getSingleResult();
+        System.out.println("close");
+        em.close();
+
+        return p;
+    }
+
+    public void Consulta() {
+        // Recupera el entity manager
+        EntityManager em = new EM_Controller().getEntityManager();
+
+        System.out.println("Consulta");
+        //List<Persona> lista = (List<Persona>) em.createQuery("FROM Persona").getResultList();
+        Query q = em.createQuery("FROM Asseguradora");
+        List<Asseguradora> lista = (List<Asseguradora>) q.getResultList();
+        imprimirLista(lista);
+
+        System.out.println("close");
+        em.close();
+    }
+
+    public void imprimirLista(List<Asseguradora> lista) {
+        System.out.println("Numero d'asseguradores= " + lista.size());
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+        }
+    }
 }
